76ad03b9b73a2653a08b34a6e36c74e0b3903b60
Icaro <icaromorone@gmail.com>
2023-01-21 09:23:12 -0300

    variável

---
+++ home/jogos/estruturar/prog_estruturada/programa.py
@@ -0,0 +1,21 @@
+# usando a função print
+print("\nMinha Primeira Aplicaçao Python\n")
+
+# criando uma variável
+mensagem = 'ola!'
+numero = 2
+pi = 3.14
+print(mensagem)
+print(numero)
+print(pi)
+
+# Metodos upper, lower capitalize
+
+# metodo upper deixa todas as letras maiúsculas
+print(mensagem.upper())
+
+# metodo lower deixa todas as letra minúsculas
+print(mensagem.lower())
+
+# metodo capitalize faz a primeira letra da palavra ficar maiúscula
+print(mensagem.capitalize())
